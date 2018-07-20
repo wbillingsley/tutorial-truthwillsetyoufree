40ab36fb641c138d4b76020b9552c0542064c0d2
Will Billingsley <wbilling@une.edu.au>
2018-07-20 11:35:39 +1000

    Added some more comments into Notepad.addPerson

--- src/main/java/com/wbillingsley/tutorials/classdiags/Notepad.java
+++ src/main/java/com/wbillingsley/tutorials/classdiags/Notepad.java
@@ -8,17 +8,31 @@ import java.util.Map.Entry;
  * Used for writing down identities.
  */
 public class Notepad {
-	
+
+	/**
+	 * For those who haven't seen them before, Maps in Java relate a key to a value.
+	 * So, this lets you look up a Person by their number...
+	 */
 	private HashMap<Integer, Person> forwardMap = new HashMap<Integer, Person>();
 
+	/**
+	 * ...and this lets you look up a Person's number by using the Person themselves as the key.
+	 */
 	private HashMap<Person, Integer> reverseMap = new HashMap<Person, Integer>();
 	
 	/**
 	 * Adds a person into the notebook. If the person was already in there, check they still claim to have the same number
 	 */
 	public void addPerson(int number, Person p) {
+		// Put the person into the map from number to Person
 		forwardMap.put(number, p);
+
+		// Put the person into the map from Person to number.
+		// When we call put, if there was already a value for that key (a number for that Person), it will return it
+		// (that's just how the Java library writers wrote HashMap)
 		Integer i = reverseMap.put(p, number);
+
+		// If they already had a number and it was different, they have lied!
 		if (i != null && i != number) {
 			throw new LiarException(i, number, p);
 		}
